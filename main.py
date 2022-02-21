38a18e97203583840223251d7cb04e25e1b87688
Maverick <tayqinyuan@gmail.com>
2022-02-21 09:27:45 +0000

    fixed pygame screen not showing up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 pygame.init()
 
 class DrawInformation:
@@ -11,6 +12,7 @@ class DrawInformation:
   BACKGROUND_COlOR = WHITE
 
   SIDE_PAD = 100
+  TOP_PAD = 150
 
   def __init__(self, width, height, lst):
     self.width = width
@@ -38,3 +40,29 @@ def generate_starting_list(n, min_val, max_val):
     lst.append(val)
   
   return lst
+
+def main():
+  run = True
+  clock = pygame.time.Clock()
+
+  n = 50
+  min_val = 0
+  max_val = 100
+
+  lst = generate_starting_list(n, min_val, max_val)
+  draw_info = DrawInformation(800, 600, lst)
+
+  while run:
+    clock.tick(60)
+
+    pygame.display.update()
+
+    for event in pygame.event.get():
+      if event == pygame.QUIT:
+        run = False
+
+  pygame.quit()
+
+
+if __name__ == "__main__":
+	main()
